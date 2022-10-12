9c44cd1837cb79a8357c2eaed7ad933742fec579
alikamal2381 <alikamal2381@gmail.com>
2022-10-12 23:53:57 +0500

    final2

---
+++ script.bat
@@ -0,0 +1,14 @@
+echo "# alikamal-portfolio" >> README.md
+git init
+git add README.md
+
+git add .gitignore
+git add package.json
+git add public/
+
+
+
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/alikamal2381/alikamal-portfolio.git
+git push -u origin main
